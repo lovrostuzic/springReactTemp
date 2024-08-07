77155cff73f4af300051327cd80bf59315f9b5a4
lovrostuzic <goldstudiostu@gmail.com>
2024-08-07 13:05:30 +0200

    Initial commit

---
+++ src/main/java/com/example/springreacttemp/SpringReactTempApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreacttemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactTempApplication.class, args);
+    }
+
+}
